8110aceb77e36c9927e9d19115094ce7b8e87c39
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-05-23 07:16:57 +0200

    Update login redirect url

--- teamstoer/settings/base.py
+++ teamstoer/settings/base.py
@@ -175,3 +175,5 @@ WAGTAIL_SITE_NAME = "teamstoer"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://www.teamstoer.nl'
+
+LOGIN_REDIRECT_URL = '/admin'
